85f032a198f4f2a96a31bbe069c5ba44bf3fbcd9
Noah K <noah171202@gmail.com>
2019-03-21 22:47:07 +0400

    loli stuff

--- nekometa.py
+++ nekometa.py
@@ -48,22 +48,30 @@ async def help(ctx):
 @bot.event
 async def on_message(message):
     if bot.user.id != message.author.id:
+        if bot.user.id in message.content:
+            await bot.send_message(message.channel, 'Talking bout me huh?')
+        if 'Hru' in message.content:
+            bot.send_message(message.channel, 'Im doing good, wbu?:3')
         if 'UwU' in message.content:
             if message.author.id == '411715164228812801':
                 await bot.send_message(message.channel, 'hewo miuu:3')
             else:
-                await bot.send_message(message.channel, 'OwO senpai:p')
+                await bot.send_message(message.channel, 'OwO senpai')
         if 'oof' in message.content:
             await bot.send_message(message.channel, 'Oof^^')
         if 'OwO' in message.content:
             if message.author.id == '502521437496803359':
                 await bot.send_message(message.channel, 'OwO master~')
             if message.author.id == '411715164228812801':
-                await bot.send_message(message.channel, 'u.u miu^^')
+                await bot.send_message(message.channel, 'miu.u')
         if 'Oof' in message.content:
             await bot.send_message(message.channel, 'Oof^^')
-        if message.content.startswith('Gay'):
-            await bot.send_message(message.channel, 'no u:p')
+        if 'Gay' in message.content:
+            if bot.user.id in message.content:
+                await bot.send_message(message.channel, 'No u')
+        if 'gay' in message.content:
+            if bot.user.id in message.content:
+                await bot.send_message(message.channel, 'No u')
         if 'No u' in message.content:
             await bot.send_message(message.channel, 'no no u')
         if 'No no u' in message.content:
@@ -82,6 +90,10 @@ async def on_message(message):
             await bot.send_message(message.channel, 'N-word detected----_Alerting <@!502521437496803359>_')
         if 'Ngl' in message.content:
             await bot.send_message(message.channel, 'N-word detected----_Alerting <@!502521437496803359>_')
+        if 'loli' in message.content:
+            await bot.send_message(message.channel, 'Did someone say _LOLIS_?')
+        if 'Loli' in message.content:
+            await bot.send_message(message.channel, 'Did someone say _LOLIS_?')
     await bot.process_commands(message)
 
 bot.run('NTU2MTIxMTMxMTI0NTIzMDA4.D22SBg.1aHUkLAbjNUkF8F46OvkWUgMLoM')
